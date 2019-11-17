8f16027918410afeaa4ebe6586e76b5de72a75e3
sevimcode <sevimsuruci@gmail.com>
2019-11-16 21:31:38 -0600

    first commit from master

--- Project/src/stringManipulationProject/AssignmentsSkyView.java
+++ Project/src/stringManipulationProject/AssignmentsSkyView.java
@@ -1,4 +1,44 @@
+// Hi SkyView,
+//This is our master branch project please find your assignment under your name and create a new branch for yourself
+// put your name for your new branch
+// work on your assignment when you done with your code push your code to master branch
+//All assignments are java interview questions
+
+//Sevim - Create a method to return the maximum occurring character in given string
+
+//Rana - Create a method to return  non repeated characters from String.
+
+//Asel -  Create a method to find numbers in a string and return the sum of all digits in the string
+
+// Aisuluu -  Create a method to check if a given String contains valid parentheses
+//he brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not
+
+//Dymtro - Create a method to remove all duplicates character in a given string remove them and return the new string
+
+// Doka - Create method reverse given string and return reverse string
+
+//Bagdad - Create a method that checks the string is palindrome or not return true or false
+
+//Alex - Create a method that return counts of number of vowels and number of consonants in a given string.
+
+//Good luck with your assignments!:)
+
+// Please finish them by Tuesday 5 pm.
+
+
 package stringManipulationProject;
 
 public class AssignmentsSkyView{
+
+
+    public static void main(String[] args){
+
+        String word = "ssskyyvv{iewwwwwww{]wwwqqaaa$%&nnngtetssh!!!><skyviewwillrockinterviews!}";
+
+    }
+
+
+
+
+
 }
